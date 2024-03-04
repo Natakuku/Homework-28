5fecbc0889ce022e9354d6c6a623f19f3a549c31
“Nataliia” <kukobnikova@gmail.com>
2024-03-04 17:20:36 +0200

    All changes committed

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 'use strict';
 
-const imageNames = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg'];
-const randomNumber = Math.floor(Math.random() * imageNames.length);
-const randomImagePath = `./images/${imageNames[randomNumber]}`;
-const randomImageElement = document.getElementById('randomImage');
-randomImageElement.src = randomImagePath;
+let image = document.getElementById('dynamicImage');
+let randomNumber = Math.floor(Math.random() * 9) + 1;
+let imagePath = `./images/${randomNumber}.jpg`;
+image.setAttribute('src', imagePath);
